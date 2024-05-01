1bddbaebd1f924539d1321b87b2d19142a8a4b48
Micah Halter <micah@mehalter.com>
2024-05-01 11:37:38 -0400

    chore: simplify disabled plugins

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -33,8 +33,8 @@ return {
   -- disable right most mode indicator
   { "heirline.nvim", opts = function(_, opts) opts.statusline[#opts.statusline] = nil end },
   -- disabled plugins
-  { "lukas-reineke/indent-blankline.nvim", enabled = false }, -- indentation levels
-  { "max397574/better-escape.nvim", enabled = false }, -- disable `jk` and `jj` for escape
-  { "nvimtools/none-ls.nvim", enabled = false }, -- replaced with conform and nvim-lint
-  { "jay-babu/mason-null-ls.nvim", enabled = false }, -- no longer needed
+  { "indent-blankline.nvim", enabled = false }, -- indentation levels
+  { "better-escape.nvim", enabled = false }, -- disable `jk` and `jj` for escape
+  { "none-ls.nvim", enabled = false }, -- replaced with conform and nvim-lint
+  { "mason-null-ls.nvim", enabled = false }, -- no longer needed
 }
