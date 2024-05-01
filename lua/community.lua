1995f5e21bd3949fa284ecf3c1fe34bde3225854
Micah Halter <micah@mehalter.com>
2024-05-01 11:10:28 -0400

    feat: show relative path in file window bar

--- lua/community.lua
+++ lua/community.lua
@@ -5,13 +5,15 @@ return {
 
   -- Add Mason Tool Installer and initialize an empty ensure_installed list
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
-  { "mason-tool-installer.nvim", opts = { ensure_installed = {} } },
 
   -- Add conform.nvim for more stable formatting
   { import = "astrocommunity.editing-support.conform-nvim" },
 
   -- Add conform.nvim for more stable linting
   { import = "astrocommunity.lsp.nvim-lint" },
+
+  -- Show relative path in winbar
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
 
   -- Show mode text in the statusline
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
