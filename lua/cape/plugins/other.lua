da6ab2dfe5bb91c7613f5597a8c2c027c9630c75
Micah Halter <micah@mehalter.com>
2024-05-03 13:17:35 -0400

    feat: add clock to statusline

--- lua/cape/plugins/other.lua
+++ lua/cape/plugins/other.lua
@@ -32,15 +32,58 @@ return {
   },
   -- setup custom left only mode text indicator
   {
+    "astroui",
+    ---@type AstroUIOpts
+    opts = {
+      icons = {
+        Clock = "", -- add icon for clock
+      },
+      status = {
+        attributes = {
+          mode = { bold = true }, -- bold mode/clock text
+        },
+      },
+    },
+  },
+  {
     "heirline.nvim",
     opts = function(_, opts)
       local status = require "astroui.status"
-      opts.statusline[1] = status.component.mode {
-        mode_text = { padding = { left = 1 } },
-        hl = { bold = true },
-        surround = { separator = { "", " " } },
-      } -- add the mode text with custom separator and bold highlighting
-      opts.statusline[#opts.statusline] = nil -- remove right mode indicator
+      -- add the mode text with custom separator and bold highlighting
+      opts.statusline[1] =
+        status.component.mode { mode_text = { padding = { left = 1 } }, surround = { separator = { "", " " } } }
+      -- add a clock to the right hand side
+      opts.statusline[#opts.statusline] = status.component.builder {
+        {
+          provider = function()
+            local time = os.date "%H:%M" -- show hour and minute in 24 hour format
+            ---@cast time string
+            return status.utils.stylize(time, {
+              icon = { kind = "Clock", padding = { right = 1 } }, -- add icon
+              padding = { right = 1 }, -- pad the right side
+            })
+          end,
+        },
+        update = { "ModeChanged", pattern = "*:*", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+        hl = status.hl.get_attributes "mode", -- highlight based on mode attributes
+        surround = {
+          separator = { " ", "" },
+          color = status.hl.mode_bg,
+          update = { "ModeChanged", pattern = "*:*" },
+        }, -- background highlight based on mode
+        init = status.init.update_events {
+          { "User", pattern = "UpdateTime", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+        },
+      }
+
+      local uv = vim.uv or vim.loop
+      uv.new_timer():start( -- timer for updating the time
+        (60 - tonumber(os.date "%S")) * 1000, -- offset timer based on current seconds past the minute
+        60000, -- update every 60 seconds
+        vim.schedule_wrap(
+          function() vim.api.nvim_exec_autocmds("User", { pattern = "UpdateTime", modeline = false }) end
+        )
+      )
     end,
   },
   -- disabled plugins
